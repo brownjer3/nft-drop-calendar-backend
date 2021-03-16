ba4e910898216383e0112b9df3314c44bf3c29b3
jerry <brownjer3@gmail.com>
2021-03-16 16:28:21 -0500

    index controller action for events api

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,11 @@
 class EventsController < ApplicationController
+
+    def index
+        events = Event.all
+        render json: events.to_json()
+    end
+
+    def show
+        # event = Event.find(id: params[:id])
+    end
 end
