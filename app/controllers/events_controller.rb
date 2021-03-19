1c83ac457ecac931a5b31da2489833ae4362028c
jerry <brownjer3@gmail.com>
2021-03-18 20:03:23 -0500

    add create controller action for events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,10 +2,30 @@ class EventsController < ApplicationController
 
     def index
         events = Event.all
-        render json: events.to_json()
+        render json: events
     end
 
-    def show
-        # event = Event.find(id: params[:id])
+    def create 
+        binding.pry
+        event = Event.new(event_params)
+        
+        if event.save
+            render json: event
+        else
+            render json: {error: "Unable to save event"}
+        end
     end
+
+    private
+    def event_params 
+        params.require(:event).permit(:title, :creator_name, :platform_name, :asset_url, :drop_date, :drop_time, :type_name)
+    end 
+
+    # state = {
+    #     title: "", 
+    #     creator_name: "", 
+    #     platform_name: "", 
+    #     asset_url: "", 
+    #     drop_date: "", 
+    #     type_name: ""
 end
