0d6751a08a8e30def7011194718ae1f1a3bf1817
jerry <brownjer3@gmail.com>
2021-03-23 10:13:51 -0700

    change drop_datetime attribute to a simple Time string

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,13 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-# require "time"
-require "date"
+require "time"
+# require "date"
 
 events = Event.create([
     {
         asset_url: "https://d9hhrg4mnvzow.cloudfront.net/on.wax.io/nimoy/78201d67-nimoy-cards-base-front-1_10a60e8000000000000028.png",
-        drop_datetime: DateTime.parse('March 25 2021 13:00'),
+        drop_datetime: Time.parse('March 25 2021 13:00'),
         title: "Leonard Nimoy collectibles",
         creator_name: "Leonard Nimoy's family", 
         platform_name: "WAX", 
@@ -20,7 +20,7 @@ events = Event.create([
         type_name: "Trading Cards"
     }, {
         asset_url: "https://cartombs.io/wp-content/uploads/2021/03/17.-Gutbuster.png",
-        drop_datetime: DateTime.parse('Mar 27 2021 13:00'),
+        drop_datetime: Time.parse('Mar 27 2021 13:00'),
         # drop_date: Date.parse("Mar 27 2021"),
         title: "Cartombs",
         creator_name: "Rory McQueen", 
@@ -30,7 +30,7 @@ events = Event.create([
         type_name: "Trading Cards"
     }, {
         asset_url: "https://res.cloudinary.com/nifty-gateway/image/upload/q_jpegmini,w_1500/v1616300210/A/FelipePantone/pjimage_-_2021-03-21T001646.201_sxlc1x.jpg",
-        drop_datetime: DateTime.parse('Mar 24 2021 19:30'),
+        drop_datetime: Time.parse('Mar 24 2021 19:30'),
         # drop_date: Date.parse("Mar 24 2021"),
         title: "The Graffiti Collection",
         creator_name: "Felipe Pantone", 
